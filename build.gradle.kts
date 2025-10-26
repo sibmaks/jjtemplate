b00a89cd8d4df45f1b9c714e58071ef0bfb4c2c9
sibmaks <sibmaks@vk.com>
2025-10-27 00:47:29 +0300

    Change stage uri

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,7 +116,7 @@ subprojects {
         repositories {
             maven {
                 name = "Staging"
-                url = uri("${rootProject.layout.buildDirectory}/staging-deploy")
+                url = uri(rootProject.layout.buildDirectory.dir("staging-deploy"))
             }
         }
     }
@@ -181,7 +181,7 @@ publishing {
     repositories {
         maven {
             name = "Staging"
-            url = uri("${rootProject.layout.buildDirectory}/staging-deploy")
+            url = uri(rootProject.layout.buildDirectory.dir("staging-deploy"))
         }
     }
 }
