97bcd47ac172237a21d23d3f42883d801fd46a00
sibmaks <sibmaks@vk.com>
2025-11-18 10:25:31 +0300

    Add sum/sub math functions
    Signed-off-by: sibmaks <sibmaks@vk.com>

---
+++ jjtemplate-evaluator/src/main/java/io/github/sibmaks/jjtemplate/evaluator/fun/impl/math/SubTemplateFunction.java
@@ -0,0 +1,88 @@
+package io.github.sibmaks.jjtemplate.evaluator.fun.impl.math;
+
+import io.github.sibmaks.jjtemplate.evaluator.fun.TemplateFunction;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @since 0.4.0
+ */
+public class SubTemplateFunction implements TemplateFunction<Number> {
+
+    private static BigDecimal toBigDecimal(Number value) {
+        if (value instanceof BigDecimal) {
+            return (BigDecimal) value;
+        }
+        if (value instanceof BigInteger) {
+            return new BigDecimal((BigInteger) value);
+        }
+        return BigDecimal.valueOf(value.doubleValue());
+    }
+
+    private static BigInteger toBigInteger(Number value) {
+        if (value instanceof BigInteger) {
+            return (BigInteger) value;
+        }
+        return BigInteger.valueOf(value.longValue());
+    }
+
+    private Number sum(Object left, Object right) {
+        if (left == null || right == null) {
+            return null;
+        }
+        if (!(left instanceof Number) || !(right instanceof Number)) {
+            throw fail("not a number passed");
+        }
+
+        var l = (Number) left;
+        var r = (Number) right;
+
+        if (l instanceof BigDecimal || r instanceof BigDecimal) {
+            return toBigDecimal(l).subtract(toBigDecimal(r));
+        }
+
+        if (l instanceof BigInteger || r instanceof BigInteger) {
+            return toBigInteger(l).subtract(toBigInteger(r));
+        }
+
+        if (l instanceof Double || r instanceof Double || l instanceof Float || r instanceof Float) {
+            return toBigDecimal(l).subtract(toBigDecimal(r));
+        }
+
+        return toBigInteger(l).subtract(toBigInteger(r));
+    }
+
+    @Override
+    public Number invoke(List<Object> args, Object pipeArg) {
+        if (args.size() != 1) {
+            throw fail("too much arguments passed");
+        }
+        var left = args.get(0);
+        return sum(left, pipeArg);
+    }
+
+    @Override
+    public Number invoke(List<Object> args) {
+        if (args.isEmpty()) {
+            throw fail("2 arguments required");
+        }
+        if (args.size() != 2) {
+            throw fail("too much arguments passed");
+        }
+        var left = args.get(0);
+        var right = args.get(1);
+        return sum(left, right);
+    }
+
+    @Override
+    public String getNamespace() {
+        return "math";
+    }
+
+    @Override
+    public String getName() {
+        return "sub";
+    }
+}
---
+++ jjtemplate-evaluator/src/test/java/io/github/sibmaks/jjtemplate/evaluator/fun/impl/math/SubTemplateFunctionTest.java
@@ -0,0 +1,181 @@
+package io.github.sibmaks.jjtemplate.evaluator.fun.impl.math;
+
+import io.github.sibmaks.jjtemplate.evaluator.exception.TemplateEvalException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author sibmaks
+ */
+@ExtendWith(MockitoExtension.class)
+class SubTemplateFunctionTest {
+    @InjectMocks
+    private SubTemplateFunction function;
+
+
+    @Test
+    void checkNamespace() {
+        assertEquals("math", function.getNamespace());
+    }
+
+    @Test
+    void checkName() {
+        assertEquals("sub", function.getName());
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void subAsArgs(Number left, Number right, Number expected) {
+        var args = List.<Object>of(left, right);
+        var result = function.invoke(args);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void subAsPipe(Number left, Number right, Number expected) {
+        var args = List.<Object>of(left);
+        var result = function.invoke(args, right);
+        assertEquals(expected, result);
+    }
+
+    public static Stream<Arguments> cases() {
+        return Stream.of(
+                Arguments.of(-1, 1, BigInteger.valueOf(-2)),
+                Arguments.of(1, -1, BigInteger.valueOf(2)),
+                Arguments.of(1, 1, BigInteger.valueOf(0)),
+                Arguments.of(-1, -1, BigInteger.valueOf(0)),
+
+                Arguments.of(BigInteger.valueOf(-1), BigInteger.valueOf(1), BigInteger.valueOf(-2)),
+                Arguments.of(BigInteger.valueOf(1), BigInteger.valueOf(-1), BigInteger.valueOf(2)),
+                Arguments.of(BigInteger.valueOf(1), BigInteger.valueOf(1), BigInteger.valueOf(0)),
+                Arguments.of(BigInteger.valueOf(-1), BigInteger.valueOf(-1), BigInteger.valueOf(0)),
+
+                Arguments.of(BigInteger.valueOf(-1), 1, BigInteger.valueOf(-2)),
+                Arguments.of(BigInteger.valueOf(1), -1, BigInteger.valueOf(2)),
+                Arguments.of(BigInteger.valueOf(1), 1, BigInteger.valueOf(0)),
+                Arguments.of(BigInteger.valueOf(-1), -1, BigInteger.valueOf(0)),
+
+                Arguments.of(-1, BigInteger.valueOf(1), BigInteger.valueOf(-2)),
+                Arguments.of(1, BigInteger.valueOf(-1), BigInteger.valueOf(2)),
+                Arguments.of(1, BigInteger.valueOf(1), BigInteger.valueOf(0)),
+                Arguments.of(-1, BigInteger.valueOf(-1), BigInteger.valueOf(0)),
+
+                Arguments.of(-1., 1, BigDecimal.valueOf(-2.0)),
+                Arguments.of(1., -1, BigDecimal.valueOf(2.0)),
+                Arguments.of(1., 1, BigDecimal.valueOf(0.0)),
+                Arguments.of(-1., -1, BigDecimal.valueOf(0.0)),
+
+                Arguments.of(-1.f, 1, BigDecimal.valueOf(-2.0)),
+                Arguments.of(1.f, -1, BigDecimal.valueOf(2.0)),
+                Arguments.of(1.f, 1, BigDecimal.valueOf(0.0)),
+                Arguments.of(-1.f, -1, BigDecimal.valueOf(0.0)),
+
+                Arguments.of(BigDecimal.valueOf(-1.), 1, BigDecimal.valueOf(-2.0)),
+                Arguments.of(BigDecimal.valueOf(1.), -1, BigDecimal.valueOf(2.0)),
+                Arguments.of(BigDecimal.valueOf(1.), 1, BigDecimal.valueOf(0.0)),
+                Arguments.of(BigDecimal.valueOf(-1.), -1, BigDecimal.valueOf(0.0)),
+
+                Arguments.of(-1, 1., BigDecimal.valueOf(-2.0)),
+                Arguments.of(1, -1., BigDecimal.valueOf(2.0)),
+                Arguments.of(1, 1., BigDecimal.valueOf(0.0)),
+                Arguments.of(-1, -1., BigDecimal.valueOf(0.0)),
+
+                Arguments.of(-1, 1.f, BigDecimal.valueOf(-2.0)),
+                Arguments.of(1, -1.f, BigDecimal.valueOf(2.0)),
+                Arguments.of(1, 1.f, BigDecimal.valueOf(0.0)),
+                Arguments.of(-1, -1.f, BigDecimal.valueOf(0.0)),
+
+                Arguments.of(-1., 1., BigDecimal.valueOf(-2.0)),
+                Arguments.of(1., -1., BigDecimal.valueOf(2.0)),
+                Arguments.of(1., 1., BigDecimal.valueOf(0.0)),
+                Arguments.of(-1., -1., BigDecimal.valueOf(0.0)),
+
+                Arguments.of(-1., BigDecimal.valueOf(1.), BigDecimal.valueOf(-2.0)),
+                Arguments.of(1., BigDecimal.valueOf(-1.), BigDecimal.valueOf(2.0)),
+                Arguments.of(1., BigDecimal.valueOf(1.), BigDecimal.valueOf(0.0)),
+                Arguments.of(-1., BigDecimal.valueOf(-1.), BigDecimal.valueOf(0.0)),
+
+                Arguments.of(-1.f, 1.f, BigDecimal.valueOf(-2.0)),
+                Arguments.of(1.f, -1.f, BigDecimal.valueOf(2.0)),
+                Arguments.of(1.f, 1.f, BigDecimal.valueOf(0.0)),
+                Arguments.of(-1.f, -1.f, BigDecimal.valueOf(0.0)),
+
+                Arguments.of(BigDecimal.valueOf(-1.), BigDecimal.valueOf(1.), BigDecimal.valueOf(-2.0)),
+                Arguments.of(BigDecimal.valueOf(1.), BigDecimal.valueOf(-1.), BigDecimal.valueOf(2.0)),
+                Arguments.of(BigDecimal.valueOf(1.), BigDecimal.valueOf(1.), BigDecimal.valueOf(0.0)),
+                Arguments.of(BigDecimal.valueOf(-1.), BigDecimal.valueOf(-1.), BigDecimal.valueOf(0.0)),
+
+                Arguments.of(BigInteger.valueOf(-1), BigDecimal.valueOf(1.), BigDecimal.valueOf(-2.0)),
+                Arguments.of(BigDecimal.valueOf(1.), BigInteger.valueOf(-1), BigDecimal.valueOf(2.0)),
+                Arguments.of(BigInteger.valueOf(1), BigDecimal.valueOf(1.), BigDecimal.valueOf(0.0)),
+                Arguments.of(BigDecimal.valueOf(-1.), BigInteger.valueOf(-1), BigDecimal.valueOf(0.0))
+
+        );
+    }
+
+    @Test
+    void subNullValue() {
+        var args = new ArrayList<>();
+        args.add(null);
+        args.add(5);
+        var result = function.invoke(args);
+        assertNull(result);
+    }
+
+    @Test
+    void subNullPipe() {
+        var args = List.<Object>of(10);
+        var result = function.invoke(args, null);
+        assertNull(result);
+    }
+
+    @Test
+    void invalidArgsType() {
+        var args = List.<Object>of("not", 1);
+        var exception = assertThrows(TemplateEvalException.class, () -> function.invoke(args));
+        assertEquals("math:sub: not a number passed", exception.getMessage());
+    }
+
+    @Test
+    void invalidPipeArgType() {
+        var args = List.<Object>of(1);
+        var exception = assertThrows(TemplateEvalException.class, () -> function.invoke(args, "x"));
+        assertEquals("math:sub: not a number passed", exception.getMessage());
+    }
+
+    @Test
+    void tooManyArgsInInvoke() {
+        var args = List.<Object>of(1, 2, 3);
+        var exception = assertThrows(TemplateEvalException.class, () -> function.invoke(args));
+        assertEquals("math:sub: too much arguments passed", exception.getMessage());
+    }
+
+    @Test
+    void tooFewArgsInInvoke() {
+        var args = List.of();
+        var exception = assertThrows(TemplateEvalException.class, () -> function.invoke(args));
+        assertEquals("math:sub: 2 arguments required", exception.getMessage());
+    }
+
+    @Test
+    void tooManyArgsInPipeInvoke() {
+        var args = List.<Object>of(1, 2);
+        var exception = assertThrows(TemplateEvalException.class, () -> function.invoke(args, 3));
+        assertEquals("math:sub: too much arguments passed", exception.getMessage());
+    }
+}
