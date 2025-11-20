2c66f0614594556cfa3d7a8ca553b587728b3e55
Drobyshev Maksim <sibmaks@vk.com>
2025-11-20 08:08:31 +0300

    Update StringTrimTemplateFunction.java

--- jjtemplate-evaluator/src/main/java/io/github/sibmaks/jjtemplate/evaluator/fun/impl/string/StringTrimTemplateFunction.java
+++ jjtemplate-evaluator/src/main/java/io/github/sibmaks/jjtemplate/evaluator/fun/impl/string/StringTrimTemplateFunction.java
@@ -3,7 +3,6 @@ package io.github.sibmaks.jjtemplate.evaluator.fun.impl.string;
 import io.github.sibmaks.jjtemplate.evaluator.fun.TemplateFunction;
 
 import java.util.List;
-import java.util.Locale;
 
 /**
  * @author sibmaks
